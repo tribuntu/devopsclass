71a8b6dc411e00b88305f66329f703f339308828
tribuntu <tribuntu@gmail.com>
2022-09-06 11:14:50 +0300

    Initial commit for vagrant lab

---
+++ vagrant/Manual_provisioning/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"b85b2fdb861fc74d6eb6631bbad65ea623595802220fc5e5bde70e2c0b21abd8","vagrant_version":"2.2.19"}
